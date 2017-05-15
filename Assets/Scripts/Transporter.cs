cd909a016ee8a78124f9abbcb5210c4a4dd72823
Unknown <soohoos1@hawkmail.newpaltz.edu>
2017-05-15 12:16:10 -0400

    Attempted to add pipe transporter (with bug) - FINAL

---
+++ Assets/Scripts/Transporter.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Transporter : MonoBehaviour {
+
+	public Transform warpTarget;
+
+	public Transform WarpTarget 
+	{
+		get {
+			return warpTarget;
+		}
+
+		set {
+			warpTarget = value;
+		}
+
+	}
+
+	void OnTriggerEnter2D (Collider2D other)
+	{
+		Debug.Log ("Object Collided");
+		other.gameObject.transform.position = WarpTarget.transform.position;
+	}
+
+}
